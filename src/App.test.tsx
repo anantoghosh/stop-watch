fc53b6f3966e6a0cdc2d0aa0e8f4e3e28a11b8f4
Ananto Ghosh <a0ghosh@gmail.com>
2018-04-25 03:25:00 +0530

    Setup testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
+import { shallow } from 'enzyme';
+import 'jest-enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  const element = shallow(<App />);
+  test('Shallow Render <App />', () => {
+    expect(element.find('.App')).toExist();
+  });
+
+  test('Match Snapshot', () => {
+    expect(element).toMatchSnapshot();
+  });
 });
